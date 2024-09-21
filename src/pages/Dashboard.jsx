dd461df392796747620ca68c714530e04df3381a
Mohamed Hamdy <m.3mawyy@gmail.com>
2024-09-21 20:13:53 +0300

    minor ui change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -43,11 +43,9 @@ const Dashboard = () => {
     }, [taskId]); // Add taskId to the dependency array
     return (
         <>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 2xl:gap-7.5">
                 <Card Icon={BucketIcon} text={'Buckets'} num={buckets?.length}
                       secNum={filterPublic(buckets)?.length}/>
-                <Card Icon={ObjectsIcon} text={'Objects'} num={435} secNum={1}/>
-                <Card Icon={FolderIcon} text={'Objects Size'} num={435} />
                 <Card Icon={KeyIcon} text={'Credentials'} num={credentialsArr?.length}/>
             </div>
             <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
